839c012ad7b2819c6a2ce2efc72a9a4540f65b8d
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2019-03-20 15:09:01 +0300

    3.4.0.0

--- AMRAdapterAdtiming/Libs/ADTiming.framework/Headers/ADTiming.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for ADTiming.
-FOUNDATION_EXPORT double ADTimingVersionNumber;
-
-//! Project version string for ADTiming.
-FOUNDATION_EXPORT const unsigned char ADTimingVersionString[];
